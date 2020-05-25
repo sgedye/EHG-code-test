b540b038fe93c138f4c3ea9ba6d89917a686dc55
Shaun Gedye <sgedye88@yahoo.com.au>
2020-05-25 11:26:04 +0800

    Updates

---
+++ precache-manifest.9a96397a26c11c16f116e66c49370c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597081f014299eb517191beada5b5966",
+    "url": "/sgedye/EHG-code-test/index.html"
+  },
+  {
+    "revision": "dde129b4a5fc1e48f59e",
+    "url": "/sgedye/EHG-code-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5ed3f50e1c65bb2834c",
+    "url": "/sgedye/EHG-code-test/static/js/2.9d78e685.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sgedye/EHG-code-test/static/js/2.9d78e685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde129b4a5fc1e48f59e",
+    "url": "/sgedye/EHG-code-test/static/js/main.5f7d80d2.chunk.js"
+  },
+  {
+    "revision": "7b0d8c54b28505664d1b",
+    "url": "/sgedye/EHG-code-test/static/js/runtime-main.8c231ea8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sgedye/EHG-code-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
