9cf3398182f881bc7b57120eaf73adb76aa8a4a1
Shaun Gedye <sgedye88@yahoo.com.au>
2020-05-25 22:11:04 +0800

    end of day commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,81 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Table from './components/table';
 import './App.css';
 
 export default function App() {
+  
+  //let [hexCode, setHexCode] = useState('')
+  // let code = 'A8A808'
+
+  useEffect(() => {
+  //   const getNextHexColour = () => {
+  //     // if (!hexCode) {
+  //     //   setHexCode = 'F80808'
+  //     // } else {
+  //     //   setHexCode = (parseInt(hexCode, 16) + 8).toString(16)
+  //     // }
+  //     code = (parseInt(code) + 8).toString(16)
+  //     console.log(code)
+  //     return `#${code}`
+  //   }
+
+  //   const tableBody = document.getElementById('colour-table').children[0]
+  //   for (let i=0; i<144; i++) {
+  //     let row = Math.floor(i/12)
+  //     let col = i % 12
+  //     tableBody.children[row].children[col].style.background = getNextHexColour()
+  //   }
+    //colourTable()
+    const tableBody = document.getElementById('colour-table').children[0]
+
+    console.log("hi")
+    console.log(tableBody)
+    // console.log(tableBody.children[0].children[0].style.height = '20px')
+    // tableBody.children[0].children[3].style.backgroundColor = 'red'
+   
+    console.log('--------')
+
+  //   let x = document.getElementById('colour-table')
+  //   console.log(x.children[0].children[5].children[3].style.background = 'red')
+    
+  //   x.children[0].children[5].children[5].style.background='blue'
+  //   let colour = 'blue'
+  //   for (let i=0; i<5; i++) {
+  //     x.children[0].children[3].children[i].style.background = getNextHexColour(i)
+  //   }
+  }, [])
+
+  function increasingHex() {
+    const tableBody = document.getElementById("colour-table").children[0]
+    for (let i = 0; i < 32768; i++) {
+      const redValue = (8 * Math.floor(i / 1024)).toString(16).padStart(2, "0")
+      const greenValue = (8 * (Math.floor(i / 32) % 32)).toString(16).padStart(2, "0")
+      const blueValue = (8 * (i % 32)).toString(16).padStart(2, "0")
+      tableBody.children[Math.floor(i / 32)].children[i % 32]
+        .style.backgroundColor = `#${redValue}${greenValue}${blueValue}`
+    }
+  }
+  const decresingHex = () => {
+    const tableBody = document.getElementById("colour-table").children[0]
+    let count = 32768
+    for (let i = 0; i < 32768; i++) {
+      count--
+      console.log(i, tableBody)
+      const redValue = (8 * Math.floor(count / 1024)).toString(16).padStart(2, "0")
+      const greenValue = (8 * (Math.floor(count / 32) % 32)).toString(16).padStart(2, "0")
+      const blueValue = (8 * (count % 32)).toString(16).padStart(2, "0")
+      tableBody.children[Math.floor(i / 32)].children[i % 32]
+        .style.backgroundColor = `#${redValue}${greenValue}${blueValue}`
+    }
+  }
   return (
     <div className="App">
-      <h2>This is going to be a table of 32x32 elements</h2>
+      <h2>This is going to be a table of 32x1024 elements</h2>
+      <button onClick={increasingHex}>Increasing Hex</button>
+      <button onClick={decresingHex}>Decreasing Hex</button>
+      {/* <button onClick={decresingHex}>Decreasing Hex</button> */}
       <Table />
     </div>
-  );
+  )
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,30 +1,24 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const bgColour = () => 'pink';
-
-const Td = styled.td`
-  background-color: ${bgColour};
-  width: 10px;
-  height: 10px;
-`
 
 function table() {
-
-  // let colourTable = [...new Array(32)]
+  // const hexColor = j => {
+  //   let redValue = (8 * Math.floor(j / 1024)).toString(16).padStart(2, '0')
+  //   let greenValue = (8 * (Math.floor(j / 32) % 32)).toString(16).padStart(2, '0')
+  //   let blueValue = (8 * (j % 32)).toString(16).padStart(2, '0')
+    
+  //   // console.log(j, redValue, greenValue, blueValue)
+  //   return `#${redValue}${greenValue}${blueValue}`
+  // }
   // Creating an 2D array with 256 rows and 128 columns.
-  const hexColor = j => {
-    let redValue = (8 * Math.floor(j / 1024)).toString(16).padStart(2, '0')
-    let greenValue = (8 * (Math.floor(j / 32) % 32)).toString(16).padStart(2, '0')
-    let blueValue = (8 * (j % 32)).toString(16).padStart(2, '0')
-
-    console.log(j, redValue, greenValue, blueValue)
-    return `#${redValue}${greenValue}${blueValue}`
-  }
-  const tableContents = [...new Array(256)].map((row,rowI) => (
+  const tableContents = [...new Array(1024)].map((row,rowI) => (
     <tr id={`row-${rowI}`} key={`row-${rowI}`}>
-      {[...new Array(128)].map((col, colI) => (
-        <Td key={`col-${colI}`} id={`${rowI}-${colI}`} style={{backgroundColor: hexColor(rowI * 128 + colI)}}></Td>
+      {[...new Array(32)].map((col, colI) => (
+        <td
+          key={`col-${colI}`}
+          id={`${rowI}-${colI}`}
+          //style={{backgroundColor: hexColor(rowI * 32 + colI)}}
+        >
+        </td>
       ))}
     </tr>
   ))
