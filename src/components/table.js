0727521210bd076e2ae45615f46698c573ba4d09
Shaun Gedye <sgedye88@yahoo.com.au>
2020-05-26 10:18:49 +0800

    Added useEffect hook

--- src/components/table.js
+++ src/components/table.js
@@ -1,30 +1,17 @@
 import React from 'react'
 
 function table() {
-  // const hexColor = j => {
-  //   let redValue = (8 * Math.floor(j / 1024)).toString(16).padStart(2, '0')
-  //   let greenValue = (8 * (Math.floor(j / 32) % 32)).toString(16).padStart(2, '0')
-  //   let blueValue = (8 * (j % 32)).toString(16).padStart(2, '0')
-    
-  //   // console.log(j, redValue, greenValue, blueValue)
-  //   return `#${redValue}${greenValue}${blueValue}`
-  // }
-  // Creating an 2D array with 256 rows and 128 columns.
+  // Creating an 2D array with 1024 rows and 32 columns.
   const tableContents = [...new Array(1024)].map((row,rowI) => (
     <tr id={`row-${rowI}`} key={`row-${rowI}`}>
       {[...new Array(32)].map((col, colI) => (
-        <td
-          key={`col-${colI}`}
-          id={`${rowI}-${colI}`}
-          //style={{backgroundColor: hexColor(rowI * 32 + colI)}}
-        >
-        </td>
+        <td key={`col-${colI}`} id={`${rowI}-${colI}`}></td>
       ))}
     </tr>
   ))
-
-  // let row = [...new Array(32)].map((el, i)=> <Td key={`col-${i}`}>{i}</Td>)
-  // let tableContents = [...new Array(32)].map((el,i) => <tr key={`row-${i}`}>{row}</tr>)
+  
+  // let row = [...new Array(256)].map((el, i)=> <Td key={`col-${i}`}></Td>)
+  // let tableContents = [...new Array(128)].map((el,i) => <tr key={`row-${i}`}>{row}</tr>)
 
   return (
     <table id="colour-table">
